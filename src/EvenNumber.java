eb8e92c094344561aa44f3096ad0d895c6adbba5
Amit Koshti <amitkoshti@gmail.com>
2025-06-24 18:31:11 +0200

    Session 2

--- src/EvenNumber.java
+++ src/EvenNumber.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class EvenNumber {
+    public static void main(String[] args) {
+        int i ;
+
+        for (i = 0; i < 10; i++) {
+            if(i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
 }
